3108ddabab2621d6d45beac0deef617e61f620dc
fai-songprasit <prapawan.songprasit@gmail.com>
2020-06-06 22:10:09 +1200

    create board

--- createBoard.js
+++ createBoard.js
@@ -1,5 +1,11 @@
 function createBoard (size) {
-
+    // new Array creates a row with size number of cols
+    let board = new Array(size);
+        //for every cols create new row (row = size)
+        for (i = 0; i < board.length; i++){
+            board[i] = new Array(size);
+        }
+    return board
 }
 
 module.exports = createBoard
