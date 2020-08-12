2cf307f9120b93286eca0dacc4315f41fab9e4fd
fai-songprasit <prapawan.songprasit@gmail.com>
2020-08-12 15:23:41 +1200

    check if cell is out of bounds

--- isOutOfBounds.js
+++ isOutOfBounds.js
@@ -1,5 +1,9 @@
 function isOutOfBounds (index, array) {
-
+    if (index < array.length && index >= 0){
+        return false
+    } else {
+        return true
+    }
 }
 
 module.exports = isOutOfBounds
